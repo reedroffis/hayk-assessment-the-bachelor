e76e70f090201f6af9b2221f74ae72ef85d2d74d
reedroffis <reedroffis18@gmail.com>
2020-12-19 20:23:03 -0700

    commit tree 714398fdf7534b4afdf346d949101ad398df312a

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,32 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+x = data[season].find do |contestant|
+  contestant["status"] == "Winner"  
+ end
+ return x["name"].split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+   x = contestants.find do |contestant|
+      contestant["occupation"] == occupation
+     end
+     if x != nil 
+     return x["name"]
+     end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0 
+  data.each do |season, contestants|
+    x = contestants.select do |contestant|
+      contestant["hometown"] == hometown
+    end
+    count += x.length
+  end
+  return count 
 end
 
 def get_occupation(data, hometown)
